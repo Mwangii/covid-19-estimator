47779ca5fe02dd6e3a2f5d0b222f42c4a152807f
Mwangii <41756719+Mwangii@users.noreply.github.com>
2020-04-14 14:35:13 +0300

    Update estimator.php

--- src/estimator.php
+++ src/estimator.php
@@ -10,7 +10,7 @@
 
 
 
-      <form name = "myForm"   action="src/estimator.php" method="post" >
+      <form name = "myForm"   action="/covid-19-estimator/blob/assessment/src/estimator.php" method="post" >
 
   <table bgcolor="#C4C4C4" align="center" width="380" border="0">  
 <tr>    
